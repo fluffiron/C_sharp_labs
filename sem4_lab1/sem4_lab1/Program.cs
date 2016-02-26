3c314b6aa5c2dc2d7ae5da0bf0ee0f44ec7cfe4a
Mike <blabla@bla.la>
2016-02-27 02:05:41 +0300

    2 first labs

---
+++ sem4_lab1/sem4_lab1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+
+namespace sem4_lab1
+{
+    public class lab1_class
+    {
+        private static List<string> ClassNames1 = new List<string>();
+        private static List<string> ClassNames2 = new List<string>();
+        private static Semaphore _pool;
+
+        public static void WriteInListFirst(object ThreadNum)
+        {
+            //Console.WriteLine("Start adding (first)" + ThreadNum);
+            _pool.WaitOne();
+            Thread.Sleep(10);
+            ClassNames1.Add(ThreadNum.ToString());
+            ClassNames2.Add(ThreadNum.ToString());
+            _pool.Release();
+
+            Console.WriteLine(ThreadNum + " is added in lists");
+        }
+
+        public static void WriteInListSecond(object ThreadNum)
+        {
+            //Console.WriteLine("Start adding (second)" + ThreadNum);
+            _pool.WaitOne();
+            Thread.Sleep(100);
+            ClassNames1.Add(ThreadNum.ToString());
+            ClassNames2.Add(ThreadNum.ToString());
+            _pool.Release();
+
+            Console.WriteLine(ThreadNum + " is added in lists");
+        }
+
+        private static void Main()
+        {
+            List<Thread> TreadList = new List<Thread>();
+            _pool = new Semaphore(1, 1);
+
+            for (int i = 0; i < 25; ++i)
+            {
+                Thread item1 = new Thread(WriteInListFirst);
+                Thread item2 = new Thread(WriteInListSecond);
+                TreadList.Add(item1);
+                TreadList.Add(item2);
+            }
+
+            // starting 50 threads
+            int j = 1;
+            foreach(Thread item in TreadList)
+            {
+                item.Start(j++);
+                //item.Join();
+            }
+            Console.WriteLine("Enter \"stop\"");
+            while (true)
+            {
+                string st = Console.ReadLine();
+                if (st == "stop" || st == "ыещз")
+                {
+                    break;
+                }
+            } 
+        }
+
+    }
+}
---
+++ sem4_lab2/sem4_lab2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace sem4_lab2
+{
+    class Program
+    {
+        private static volatile int x;
+        static object Lock = new object();
+        static ManualResetEvent stop = new ManualResetEvent(false);
+
+
+        public static void WriteInFile(object name)
+        {
+            FileStream fs = new FileStream(name.ToString() + ".txt", FileMode.Create);
+            fs.Close();
+            StreamWriter file = new StreamWriter(name.ToString() + ".txt", true, System.Text.Encoding.Default);
+            int _x = x;
+            while (!stop.WaitOne(30))
+            { 
+                
+                if (_x % int.Parse(name.ToString()) == 0)
+                {
+                    file.WriteLine(_x);
+                    //Console.WriteLine("Write in " + name.ToString() + ".txt file x= " + x);
+                }
+                ++_x; 
+                Monitor.TryEnter(Lock, 10);
+                if (_x >= x)
+                { 
+                    lock (Lock)
+                    {
+                        ++x;    // if none has changed x
+                    }  
+                }
+                Monitor.Exit(Lock);             
+                Thread.Sleep(10);
+            }
+            //Console.WriteLine("_x in " + name.ToString() + " is " + _x);
+            file.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            
+            x = 1;
+            List<Thread> ThreadList = new List<Thread>();
+
+            for (int i = 0; i < 10; ++i)
+            {
+                Thread item1 = new Thread(WriteInFile);
+                ThreadList.Add(item1);
+            }
+
+            // starting 10 threads
+            int j = 1;
+            foreach (Thread item in ThreadList)
+            {
+                item.Start(j++);
+            }
+            Console.WriteLine("Enter \"stop\"");
+            while (true)
+            {
+                string st = Console.ReadLine();
+                if (st == "stop" || st == "ыещз")
+                {
+                    stop.Set();
+                    break;
+                }
+            }
+            //Console.WriteLine("x is " + x);
+            //Console.ReadKey();
+        }
+    }
+}
